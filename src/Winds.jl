45e937cff5ff33c0503f6d757627da168195f46e
yakir12 <12.yakir@gmail.com>
2019-11-01 15:36:30 +0100

    dropdown for ports

--- src/Winds.jl
+++ src/Winds.jl
@@ -4,29 +4,27 @@ export main
 
 using LibSerialPort, Blink, Interact, COBS
 
-function getport(; baud = 9600)
-    for port in get_port_list()
-        sp = open(port, baud)
-        txt = LibSerialPort.sp_get_port_usb_manufacturer(sp.ref)
-        if occursin(r"arduino", txt)
-            return sp
-        end
-    end
-    error("no ports connected to an Arduino were detected...")
-end
-
-function main(; min::Int = 0, max::Int = 255)
+function main(; min::Int = 0, max::Int = 255, baud::Int = 9600)
     @assert min < max "minimum, $min, must be smaller than maximum, $max"
     @assert 0 ≤ min ≤ 255 "minimum must be between 0 and 255"
     @assert 0 ≤ max ≤ 255 "maximum must be between 0 and 255"
-    serialport = getport()
 
-    int = rangepicker(min:max, value = [min])
-    on(int) do i
-        encode(serialport, UInt8(i[]))
+    ports = get_port_list()
+    if isempty(ports)
+        error("no ports were detected...")
     end
+    dd = dropdown(ports)
+    ok = button("OK")
     w = Window()
-    body!(w, hbox("Intensity", int))
+    body!(w, hbox("Port", dd, ok))
+    on(ok) do _
+        serialport = open(dd[], baud)
+        int = rangepicker(min:max, value = [min])
+        on(int) do i
+            encode(serialport, UInt8(i[]))
+        end
+        body!(w, hbox("Intensity", int))
+    end
 end
 
 end # module
